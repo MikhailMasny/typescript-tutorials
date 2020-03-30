3d3a362f5e06eb41f40ec24686cf0065634ca176
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-03-30 22:27:34 +0300

    feat: add tutorial of types

---
+++ typescript/types.ts
@@ -0,0 +1,38 @@
+const str: string = "Hello world!";
+const bool: boolean = true; // false
+let integer: number = 123; // 1.23; 1e23
+const firstArray: number[] = [1, 2, 3];
+const secondArray: Array<number> = [1, 2, 3];
+const words: string[] = ["Hello", "TypeScript", "!"];
+const typle: [string, number] = ["Hello", 123];
+
+let variable: any = 123;
+variable = "123";
+variable = [1, 2, 3];
+
+function func(val: string): void {
+    console.log(val);
+}
+func("Hello world!");
+
+function throwError(val: string): never {
+    throw new Error(val);
+}
+
+function infinite(): never {
+    while (true) {
+
+    }
+}
+
+type MyType = string;
+const myType: MyType = "Hello world!";
+// But can't: const myType: MyType = 123;
+
+type AnyType = number | boolean;
+const anyOne: AnyType = 1;
+const anyTwo: AnyType = true;
+// But can't: const anyThree: AnyType = "Hello";
+
+type SomeType = string | null | undefined;
+const someType: SomeType = null;
